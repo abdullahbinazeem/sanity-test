6392b5b05043dc0c6497bab463ab1e46b44b4b69
Powdee123 <66191086+Powdee123@users.noreply.github.com>
2023-09-13 22:14:20 -0700

    yay

--- app/(site)/[slug]/page.tsx
+++ app/(site)/[slug]/page.tsx
@@ -1,1 +1,21 @@
-export default async function 
+import { getPage } from "@/sanity/sanity-utils";
+import { PortableText } from "@portabletext/react";
+
+interface pageParams {
+  params: { slug: string };
+}
+
+export default async function page({ params }: pageParams) {
+  const page = await getPage(params.slug);
+
+  return (
+    <div>
+      <h1 className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent text-5xl font-bold p-2">
+        {page.title}
+      </h1>
+      <div className="text-lg text-gray-700 mt-10">
+        <PortableText value={page.content} />
+      </div>
+    </div>
+  );
+}
