33a3245537eceb2e8a83ee4106cc0f77cd4492e0
Powdee123 <66191086+Powdee123@users.noreply.github.com>
2023-09-21 23:54:33 -0700

    revalidate change

--- app/(studio)/layout.tsx
+++ app/(studio)/layout.tsx
@@ -1,9 +1,6 @@
 import "../globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Link from "next/link";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
